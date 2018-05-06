0c06a99ae7788c466cd54b13dc899ff23d94523a
Vaughan Schmidt <vaughan.schmidt@gmail.com>
2018-05-06 10:39:09 -0700

    Adds saving local copy of gsheet data and backup schedule. Something wrong with event object - duplicate rooms

--- ballroomA.py
+++ ballroomA.py
@@ -15,9 +15,17 @@ import Queue
 
 CONFIG_FILE = 'config.json'
 CREDENTIALS_FILE = 'self-video-zach-208096653289.json'
+LOCAL_SCHEDULE_CACHE_FILE = 'schedule_cache.json'  # Used as fallback schedule if google sheet won't validate
+LOCAL_GSHEET_PAGE_CACHE = 'gsheet_page_cache.json' # A Copy of the state of the google sheets event page 
 SCOPE = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
+
+# Define how the Google sheet is set up
 SHEET_NAME = 'speakers-list'
+SHEET_COL_HDG_EVENT_ID = 'EventID'
+SHEET_COL_HDG_EVENT_ROOM = 'Room'
+SHEET_COL_HDG_EVENT_DATE = 'Date'
+SHEET_COL_HDG_EVENT_TIME = 'startTime'
 
 Event = collections.namedtuple('Event', 'id room date time')
 
@@ -60,28 +68,68 @@ class FeedbackCollector:
 		Returns:
 			(list of Event tuples) Current room schedule
 		'''
-
-		def validateSchedule(list s, logger):
-			cnt = count(s)
-			if cnt le 1:
-				logger.debug('Room schedule validates - 1 or fewer events!')
-			else:
-				for i = 0:
-
 		gSchedule = self.gsheet.worksheet(self.config['schedule_sheet']).get_all_records()
+
+		# save a local copy of the worksheet to filesystem.
+		# Mainly, just so somebody has all the event information locally if needed after the event.
+		with open(LOCAL_GSHEET_PAGE_CACHE, 'w') as f_out:
+			json.dump(gSchedule, f_out)
 
 		schedule = []
 		for row in gSchedule:
-			event = Event(id=row['EventID'], room=row['Room'], date=row['Date'], time=row['startTime'])
+			event = Event(id=row[SHEET_COL_HDG_EVENT_ROOM], 
+				room=row[SHEET_COL_HDG_EVENT_ROOM], 
+				date=row[SHEET_COL_HDG_EVENT_DATE], 
+				time=row[SHEET_COL_HDG_EVENT_TIME])
 			print( event.room + ' vs ' + self.config['room_id'])
 			if( event.room == self.config['room_id']):
+				logger.debug('''Adding event to schedule: id {0} in {1} on {2} @ {3}'''
+					.format(event.id, event.room, event.date, event.time))
 				schedule.append(event)
-
-		validateSchedule(schedule)
 
 		return schedule
 
-	def __init__(self, queue):
+	def validateSchedule(self, logger):
+		'''Sanity checks on room schedule / list of events
+
+		A list of room events is expected to be non-zero and no 2 events should
+		exist for same date and time.
+
+		Note: Validation fails/passes silently. Only logs the details.
+
+		Args:
+			s: (list) Room scheduled event list
+			logger: (logger) The logger to write results to
+		'''
+		cnt = len(self.roomSchedule)
+		if(cnt):
+			logger.debug('Validating {0} events in room\'s schedule.'.format(cnt))
+		else:
+			logger.error('Event schedule is empty.')
+			return
+
+		for i in xrange(0, cnt-1):
+			e1 = self.roomSchedule[i]  # The event being validated in the schedule
+			for j in xrange(i+1, cnt):
+				e2 = self.roomSchedule[j] # Some other event in the schedule
+					
+				if e1.date == e2.date and e1.time == e2.time:
+					logger.error('''Event schedule FAILS VAILIDATION. Duplicate 
+						date or time found in schedule.''')
+					return 
+			
+		logger.info('Successfully validated {0} events for roomID: \'{1}\' schedule.'
+			.format(cnt, self.config['room_id']))
+		
+		#Only save a copy of the local schedule to file system if it validated.
+		schedule = {}
+		schedule['configuration'] = self.config
+		for i in range(len(self.roomSchedule)):
+			schedule['event_{0}'.format(i)] = self.roomSchedule[i]
+		with open(LOCAL_SCHEDULE_CACHE_FILE, 'w') as f_out:
+			json.dump(schedule, f_out)
+
+	def __init__(self, queue, logger):
 		'''Instantiate new FeedbackCollector Object
 
 		Args:
@@ -94,7 +142,10 @@ class FeedbackCollector:
 		self.worksheet = self.gsheet.worksheet(self.config['room_id'])
 		self.queue = queue
 
-		self.roomSchedule = self.buildSchedule()
+		self.roomSchedule = self.buildSchedule(logger)
+
+		# Results of schedule validation will be written to log only
+		self.validateSchedule(logger)
 		
 		currenttime = datetime.now().strftime('%m_%d_%H_%M_%S')
 		self.voteLogFile = "%s_feedback.log" % str(currenttime)
@@ -130,9 +181,9 @@ def start(fc, logger):
 
 def checklist():
 		currenttime = datetime.now().strftime('%m-%d-%H:%M')
-		print "time now = ", currenttime
+		print("time now = {0}").format(currenttime)
 		if currenttime in starttimes:
-			print "current time in list = ", currenttime
+			print("current time in list = {0}").format(currenttime)
 			googlesheetlookup() 
 			exit
 	
@@ -216,7 +267,7 @@ if __name__ == "__main__":
 	queue = multiprocessing.Queue()
 
 	# Instantiate our collector and updater objects
-	collector = FeedbackCollector(queue)
+	collector = FeedbackCollector(queue, logger)
 	logger.info('FeedbackCollector object instantiated.')
 	logger.debug('FeedbackCollector: %s' % str(collector))
 
